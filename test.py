59fceb916d78de1cdd0c8f9af5508b8ea1f72913
Neil <zhao.yuxuan@foxmail.com>
2020-12-04 00:05:00 +0100

    test

--- test.py
+++ test.py
@@ -9,4 +9,22 @@ ports=range(10001, 10010)
 n1=NodeServer(ip, ports[1])
 n1.join((ip, ports[0]))
 
-
+# %%
+n2=NodeServer(ip,ports[2])
+n2.join((ip,ports[0]))
+# %%
+n3=NodeServer(ip,ports[3])
+n3.join((ip,ports[0]))
+# %%
+n1.stabilize
+n2.stabilize
+n3.stabilize
+# %%
+n1.fix_fingers
+n2.fix_fingers
+n3.fix_fingers
+# %%
+n1.update_successor_list
+n2.update_successor_list
+n3.update_successor_list
+# %%
