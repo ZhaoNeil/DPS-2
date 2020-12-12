5ba159288d31c688c9506dce04a0d8cb9f5194b5
founting <8627638+founting@users.noreply.github.com>
2020-12-12 20:58:07 +0100

    Add files via upload

---
+++ counter.py
@@ -0,0 +1,28 @@
+class StepCounter:
+  def __init__(self):
+    self.path_len={}
+
+  def update_path_len(self, keyId, nSteps):
+    if keyId not in self.path_len:
+      self.path_len[keyId]=nSteps
+
+
+class TimeoutCounter:
+  def __init__(self):
+    self.timeouts={}
+    self.nTimeout={}
+
+  def update_timeouts(self, keyId, n_):
+    if keyId not in self.timeouts:
+      self.timeouts[keyId]=[n_.addr]
+      self.nTimeout[keyId]=1
+    else:
+      if n_.addr not in self.timeouts[keyId]:
+        self.timeouts[keyId].append(n_.addr)
+        self.nTimeouts[keyId]+=1
+
+  def update_nTimeout(self, keyId, nFailed):
+    if keyId not in self.nTimeout:
+      self.nTimeout[keyId]=nFailed
+    else:
+      self.nTimeout[keyId]+=nFailed
