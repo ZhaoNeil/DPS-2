aa0c591c00083330689fa5c3e6d4afd68d07f8bb
founting <founting@users.noreply.github.com>
2020-12-16 23:27:45 +0100

    fixing

--- run.sh
+++ run.sh
@@ -2,25 +2,16 @@ var1=$(preserve -llist | grep ddps2012)
 nodes=$(echo $var1 | grep -P 'node\d+' -o)
 nodes=($nodes)
 
-ports=({10001..10020})
-addrList=()
-
-
 for i in ${!nodes[@]}
 do
 {
-    for j in ${!ports[@]}
-    do
-	{	time=$(($i+$j))
-		sleep $time
-		if [ $i -eq 0 ] && [ $j -eq 0 ]
+	sleep $i
+	if [ $i -eq 0 ]
 		then
 			rNodeAddr='None'
 		else
-			ix=$[$RANDOM % ${#addrList[@]}]
-			rNodeAddr=${addrList[$ix]}
+			rNodeAddr=(${nodes[$i]}':10001')
 		fi
-		addrList+=(${nodes[$i]}':'${ports[$j]})
 		ssh ${nodes[$i]} python3 -u ~/DPS-2/e1.py ${ports[$j]} $rNodeAddr
     }& done
 }&
