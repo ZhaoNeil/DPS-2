84f4110c219d9abf9786df7e3f7b342fa8f12926
ZhaoNeil <429199725@qq.com>
2020-12-17 00:15:34 +0100

    modified

--- result.sh
+++ result.sh
@@ -1,7 +1,4 @@
-var1=$(preserve -llist | grep ddps2012)
-nodes=$(echo $var1 | grep -P 'node\d+' -o)
-
-for node in $nodes
+for i in {001..068}
 do
-    scp $node:/home/ddps2012/DPS-2/e* ../../var/scratch/ddps2012
+    scp node$i:/home/ddps2012/DPS-2/e* ../../var/scratch/ddps2012
 done
